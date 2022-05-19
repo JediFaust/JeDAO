1d5d2e1b4a94543bfc81466fc067751f957699e5
Omur <83595409+JediFaust@users.noreply.github.com>
2022-05-20 04:31:30 +0600

    finish contract

--- contracts/JeDAO.sol
+++ contracts/JeDAO.sol
@@ -6,9 +6,6 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-
 contract JeDAO is ReentrancyGuard {
     address public chairman;
     uint256 private _minQuorum;
