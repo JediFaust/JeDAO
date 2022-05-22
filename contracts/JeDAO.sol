b1f126d53412d2f96f0e6d97c23fe18d28c195d5
Omur <83595409+JediFaust@users.noreply.github.com>
2022-05-22 08:20:51 +0600

    events added and NatSpec

--- contracts/JeDAO.sol
+++ contracts/JeDAO.sol
@@ -6,6 +6,11 @@ pragma solidity ^0.8.4;
 import "./ERC20MintableBurnable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+/// @title JeDAO contract with voting and ERC20 compatibility
+/// @author Omur Kubanychbekov
+/// @notice You can use this contract for make DAO and votings
+/// @dev All functions tested successfully and have no errors
+
 contract JeDAO is ReentrancyGuard {
     address public chairman;
     uint256 private _minQuorum;
@@ -31,7 +36,13 @@ contract JeDAO is ReentrancyGuard {
     mapping(uint256 => Proposal) private _proposals;
     mapping(address => Voter) private _voters;
 
-
+    /// @notice Deploys the contract with the initial parameters
+    /// (chairman, voteToken, minimumQuorum, debatingPeriodDuration)
+    /// @dev Constructor should be used when deploying contract,
+    /// @param chairPerson address of the chairman
+    /// @param voteToken address of the token used for voting
+    /// @param minimumQuorum minimum quorum needed for successful voting
+    /// @param debatingPeriodDuration debating period 
     constructor(
         address chairPerson,
         address voteToken,
@@ -45,18 +56,33 @@ contract JeDAO is ReentrancyGuard {
     }
 
 
+    /// @dev Modifier for addProposal function
     modifier onlyChairman {
        require(msg.sender == chairman, "Chairman only");
        _;
     }
 
+    /// @notice Event that notices about added new proposal
+    event NewProposal(uint256 indexed id, address recipient, string description);
+    
+    /// @notice Event that notices about finished proposal
+    event FinishedProposal(uint256 indexed id, bool indexed called, uint256 forVotes, uint256 againstVotes);
 
+
+    /// @notice Function that adds new proposal
+    /// @param callData of the function that will be called
+    /// should be encoded as bytes
+    /// @param _recipient address of the contract that will call the function
+    /// @return count of total proposals
     function addProposal(
         bytes memory callData,
         address _recipient,
         string memory description
     ) external onlyChairman returns(uint256) {
         Proposal storage newProposal = _proposals[_proposalID];
+
+        emit NewProposal(_proposalID, _recipient, description);
+
         _proposalID++;
 
         newProposal.finishTime = block.timestamp + _debatePeriod;
@@ -68,6 +94,12 @@ contract JeDAO is ReentrancyGuard {
     }
 
 
+    /// @notice can be called by anyone who has enough tokens deposited
+    /// @notice voting is active until finish function is called
+    /// @notice voter can add tokens to his existing deposit
+    /// and vote for proposal he voted already with newly added tokens
+    /// @param isVoteFor true if vote for, false if vote against
+    /// @return true if voting is successful
     function vote(
         uint256 proposalID,
         uint256 amount,
@@ -94,19 +126,27 @@ contract JeDAO is ReentrancyGuard {
         return true;
     }
 
+
+    /// @notice can be called by anyone to finish proposal
+    /// @notice calls the function if quorum is reached and
+    /// votes for is greater than votes against
+    /// otherwise proposal finishes with no call
+    /// @return true if proposal is finished successfully
     function finishProposal(uint256 proposalID) external returns(bool) {
         Proposal storage proposal = _proposals[proposalID];
         require(block.timestamp >= proposal.finishTime, "Proposal is not finished");
+        require(proposal.votesFor + proposal.votesAgainst >= _minQuorum, "Not enough votes");
 
-        uint256 totalVotes = proposal.votesFor + proposal.votesAgainst;
-        require(totalVotes >= _minQuorum, "Not enough votes");
+        bool isCalling = proposal.votesFor > proposal.votesAgainst;
 
-        if(proposal.votesFor > proposal.votesAgainst) {
+        if(isCalling) {
             (bool success, ) = proposal.recipient.call{value: 0}
                 (proposal.callData);
 
              require(success, "Operation failed");
         }
+
+        emit FinishedProposal(proposalID, isCalling, proposal.votesFor, proposal.votesAgainst);
 
         proposal.finishTime = 0;
 
@@ -114,6 +154,8 @@ contract JeDAO is ReentrancyGuard {
     }
 
 
+    /// @notice can be called by anyone to deposit tokens
+    /// @return true if deposit is successful
     function deposit(uint256 amount) external returns(bool) {
         _voteToken.transferFrom(msg.sender, address(this), amount);
         _voters[msg.sender].deposit += amount;
@@ -122,6 +164,8 @@ contract JeDAO is ReentrancyGuard {
     }
     
 
+    /// @notice can be called by anyone to withdraw deposited tokens
+    /// @return true if withdraw is successful
     function withdraw(uint256 amount) external nonReentrant returns(bool) {
         Voter storage voter = _voters[msg.sender];
 
